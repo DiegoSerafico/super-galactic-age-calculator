08b7c2f4ad6cac549a2402716fd889897a4c9dfe
DiegoSerafico <dseraficohernandez@gmail.com>
2021-09-03 09:15:57 -0700

    add test to calculate age on venus

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -2,8 +2,13 @@ import { AgeCalculator } from "../src/scripts";
 
 describe("AgeCalculator", () => {
 
-  test("should correctly calculate age on mercury", () => {
+  test("should correctly calculate age on Mercury", () => {
     const calculator = new AgeCalculator(1);
     expect(calculator.getAgeOnMercury()).toEqual(0.24);
+  });
+
+  test("should correctly calculate age on Venus", () => {
+    const calculator = new AgeCalculator(1);
+    expect(calculator.getAgeOnVenus()).toEqual(0.62);
   });
 });
